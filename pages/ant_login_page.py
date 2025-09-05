bb012e7205027eab595f06739de5e268cb657447
Antik-Bond <antikbond@gmail.com>
2025-09-05 14:10:29 -0400

    leave only main methods in class

--- pages/ant_login_page.py
+++ pages/ant_login_page.py
@@ -15,21 +15,22 @@ class AntLoginPage(BasePage):
         self.page_title = "h1"
         
         # Login page selectors
-        self.username_field = 'input[type="email"]'
-        self.password_field = 'input[type="password"]'
-        self.login_button = 'button[type="submit"]'
-        self.error_message = '.error-message'
-        self.username_error = '.username-error'
-        self.password_error = '.password-error'
-        self.welcome_message = '.welcome-message'
+        self.user_name = "//input[@name='username']"
+        self.password = "//input[@name='password']"
+        self.login_button = "//button[text()=' Login ']"
+        # Specify locators later
+        # self.error_message = '.error-message'
+        # self.username_error = '.username-error'
+        # self.password_error = '.password-error'
+        # self.welcome_message = '.welcome-message'
 
     def enter_username(self, username):
         """Enter username in the username field"""
-        self.fill_input(self.username_field, username)
+        self.fill_input(self.user_name, username)
 
     def enter_password(self, password):
         """Enter password in the password field"""
-        self.fill_input(self.password_field, password)
+        self.fill_input(self.password, password)
 
     def click_login_button(self):
         """Click the login button"""
@@ -40,51 +41,3 @@ class AntLoginPage(BasePage):
         self.enter_username(username)
         self.enter_password(password)
         self.click_login_button()
-
-    def get_error_message(self):
-        """Get the general error message text"""
-        return self.get_element_text(self.error_message)
-
-    def get_username_error_message(self):
-        """Get the username field error message"""
-        return self.get_element_text(self.username_error)
-
-    def get_password_error_message(self):
-        """Get the password field error message"""
-        return self.get_element_text(self.password_error)
-
-    def get_welcome_message(self):
-        """Get the welcome message after successful login"""
-        return self.get_element_text(self.welcome_message)
-
-    def is_login_button_enabled(self):
-        """Check if login button is enabled"""
-        return not self.page.is_disabled(self.login_button)
-
-    def is_error_message_visible(self):
-        """Check if error message is displayed"""
-        return self.is_element_visible(self.error_message)
-
-    def is_username_error_visible(self):
-        """Check if username error message is displayed"""
-        return self.is_element_visible(self.username_error)
-
-    def is_password_error_visible(self):
-        """Check if password error message is displayed"""
-        return self.is_element_visible(self.password_error)
-
-    def clear_username_field(self):
-        """Clear the username field"""
-        self.page.fill(self.username_field, "")
-
-    def clear_password_field(self):
-        """Clear the password field"""
-        self.page.fill(self.password_field, "")
-
-    def leave_username_empty(self):
-        """Ensure username field is empty"""
-        self.clear_username_field()
-
-    def leave_password_empty(self):
-        """Ensure password field is empty"""
-        self.clear_password_field()
