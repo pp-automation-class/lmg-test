7510d03783695b1cfe7209a29286e30de97619d6
AnKrylo <akrylova09876@gmail.com>
2025-09-12 20:44:31 -0400

    Add device locators and navigation methods in Devices Page Object Model

--- pages/ank_devices_page.py
+++ pages/ank_devices_page.py
@@ -10,4 +10,24 @@ class AnkDevicesPage(AnkBasePage):
         """Initialize with the Playwright page object"""
         super().__init__(page)
 
-        self.page_title = "//h5"
+        self.page_title = "//h3[text()='My devices ']"
+        self.device_settings_button = "//a[@href='#/device-settings']"
+        self.menu_item_devices = "//a[text()='Devices']"
+        self.menu_item_records= "//a[text()='Records']"
+        self.menu_item_logbook= "//a[text()='LogBook']"
+
+
+
+     def ank_device_locator(self, name: str):
+        """Return the locator for a specific device"""
+        return f"//div[@class='lmg-device__info']/h4[text()='{name}']"
+
+
+    def ank_open_device_settings(self):
+        self.ank_click_element(self.device_settings_button)
+
+
+    def ank_navigate_to_devices(self):
+
+
+
