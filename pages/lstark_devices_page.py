80d2c0b23f53f0031cf91ec10b79d09ee8a4f42d
Laura <laura@test.com>
2025-09-17 08:46:04 -0500

    Added notes about locators

--- pages/lstark_devices_page.py
+++ pages/lstark_devices_page.py
@@ -12,6 +12,17 @@ class LStarkDevicesPage(LStarkBasePage):
         self.page_title = "//h3[contains(text(),'My device')]"
         self.device_settings_button = "//a[@href='#/device-settings']"
 
+      # We moved ALL these to the base page since they are common for all pages
+        # self.menu_item_devices = "//a[text()='Devices']"
+        # self.menu_item_records= "//a[text()='Records']"
+        # self.menu_item_logbook = "//a[text()='Logbook']"
+        #
+        # We created function to get devices by name, but each device has unique name,
+        # so we created variable {name} to be replaced with device name we'll use
+        #
+        # def ls_get_device_name_locator(self, name: str):
+        #     """Learning: Return the locator for a specific device"""
+        #     return f"//div[@class='lmg-device__info']/h4[text()='{name}']"
 
 
 
