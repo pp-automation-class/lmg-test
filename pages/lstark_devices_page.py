15ebe992729fda97ec113eb968c983a067f087d8
Laura <laura@test.com>
2025-09-17 11:40:43 -0500

    Added notes explaining why we moved locators and function to find locators for menu_items and to click on them to the lstark_base_page.py

--- pages/lstark_devices_page.py
+++ pages/lstark_devices_page.py
@@ -12,17 +12,25 @@ class LStarkDevicesPage(LStarkBasePage):
         self.page_title = "//h3[contains(text(),'My device')]"
         self.device_settings_button = "//a[@href='#/device-settings']"
 
-      # We moved ALL these to the base page since they are common for all pages
+        # We found 3 locators for menu items below and moved them to the base_page since they are common
         # self.menu_item_devices = "//a[text()='Devices']"
         # self.menu_item_records= "//a[text()='Records']"
-        # self.menu_item_logbook = "//a[text()='Logbook']"
-        #
+        # self.menu_item_logbook = "//a[text()='LogBook']"
+
+        # and created 2 functions in the base page to work with them:
+        # 1. def ls_get_menu_item_locator(self, menu_item: str):
+        # 2. def ls_open_menu(self, menu_item: str):
+
+
         # We created function to get devices by name, but each device has unique name,
         # so we created variable {name} to be replaced with device name we'll use
         #
-        # def ls_get_device_name_locator(self, name: str):
+        # def get_device_name_locator(self, name: str):
         #     """Learning: Return the locator for a specific device"""
         #     return f"//div[@class='lmg-device__info']/h4[text()='{name}']"
+
+        # def open_device_settings(self):
+        #     self.click_element(self.device_settings_button)
 
 
 
